b2a71040354e757cd0f629678baa33107d543834
haleemausman1 <228671336+haleemausman1@users.noreply.github.com>
2026-07-05 19:59:29 +0500

    Add configuration files

---
+++ config/multerconfig.js
@@ -0,0 +1,21 @@
+// final
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images'); 
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, function (err, bytes) {
+            if (err) return cb(err);
+            const filename = bytes.toString("hex") + path.extname(file.originalname);
+            cb(null, filename); 
+        });
+    }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
